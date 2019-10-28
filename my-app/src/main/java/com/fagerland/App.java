5646d238aee5cb2f2611dfcb8aa39002be7eac93
Marius Fagerland <mariusfagerland@yahoo.no>
2019-10-28 14:03:20 +0100

    Implement ActiveMQ HelloWorld

--- my-app/src/main/java/com/fagerland/App.java
+++ my-app/src/main/java/com/fagerland/App.java
@@ -1,13 +1,16 @@
 package com.fagerland;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) throws Exception {
+        HelloWorld.helloWorld();
     }
+
 }
