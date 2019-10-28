c9a335efc075baade29a8c3df436b57b835f812a
Marius Fagerland <mariusfagerland@yahoo.no>
2019-10-28 11:52:59 +0100

    Add maven project

---
+++ activemq-research/src/main/java/com/fagerland/App.java
@@ -0,0 +1,13 @@
+package com.fagerland;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
